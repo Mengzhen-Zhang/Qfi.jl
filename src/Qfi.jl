bcdf9d156796a81b8fb76d39788ba23baf498c42
Mengzhen Zhang <mengzhen.zhang@outlook.com>
2023-03-30 17:48:01 -0500

    Add _solve to simplify the code

--- src/Qfi.jl
+++ src/Qfi.jl
@@ -4,8 +4,8 @@ import QuantumOptics
 import QuantumOptics: 
        dagger, spre, spost, ptrace
 import QuantumOptics.QuantumOpticsBase: 
-       SuperOperator, AbstractOperator
-import IterativeSolvers: bicgstabl!
+       SuperOperator, AbstractOperator, Operator
+import IterativeSolvers: bicgstabl
 import ChainRulesCore:
        frule, rrule
 import LinearAlgebra
@@ -17,49 +17,32 @@ abstract type AbstractPhySys end
 liouv(phy::AbstractPhySys)::Function = identity
 dliouv(phy::AbstractPhySys)::Function = identity
 
-function steadyState(lθ::SuperOperator)
-    M = length(lθ.basis_l[1])
-    x0 = similar(lθ.data, M^2+1)
-    x0[begin] = one(eltype(lθ.data))
-    x0[end] = zero(eltype(lθ.data))
-    
-    y = similar(lθ.data, M^2+1)
-    y[end] = one(eltype(lθ.data))
+dim_hilbert(sop::SuperOperator) = length(sop.basis_l[begin])
 
-    lm = [lθ.data  similar(lθ.data, M^2); permutedims([(i ÷ M + 1) == i % M ? one(eltype(lθ.data)) : zero(eltype(lθ.data)) for i in 1:M^2+1])]
-
-    ρθ = bicgstabl!(x0, lm, y)[1:end-1] # Super vector
-    ρθ = Operator(lθ.basis_l[1], lθ.basis_l[2], reshape(ρθ, (M, M)))
-    return (ρθ + dagger(ρθ))/2.0
+function _solve(sop::SuperOperator; 
+    bop::AbstractOperator = Operator(
+        sop.basis_l[begin], sop.basis_l[begin+1],
+        similar(sop.data, dim_hilbert(sop)^2)),
+    xin::AbstractArray=1e-6 * rand(eltype(sop.data), dim_hilbert(sop)^2), 
+    is_herm::Bool=true, is_tp::Bool=false, trace=0)
+    M = dim_hilbert(sop)
+    b = reshape(bop.data, dim_hilbert(lθ)^2)
+    dtype = eltype(sop.data)
+    x0 = [is_tp ? trace*one(dtype) - sum(xin[begin+1:end-1]) : 1e-6*rand(dtype); xin]
+    y = [b ; is_tp ? trace*one(dtype) : zero(dtype)]
+    lm = [sop.data  similar(sop.data, M^2); 
+          permutedims([(i ÷ M + 1) == i % M && is_tp ? one(dtype) : zero(dtype) for i in 1:M^2+1])]
+    svec = bicgstabl(x0, lm, y)[1:end-1] # Super vector
+    op = Operator(sop.basis_l[begin], sop.basis_l[begin+1], reshape(svec, (M, M)))
+    is_herm ? (op + dagger(op))/2.0 : op
 end
 
-function diffSteadyState(lθ::SuperOperator, dlθ::SuperOperator, ρθ::AbstractOperator)
-    M = length(dlθ.basis_l[1])
-    x0 = 1e-6 * rand(eltype(ρθ.data), M^2 + 1)
+steadyState(lθ::SuperOperator) = _solve(lθ; is_tp=true, trace=1)
 
-    y = similar(dlθ.data, M^2+1)
-    y[1:end-1].= reshape((- dlθ * ρθ).data, M^2)
-    y[end] = zero(eltype(dlθ.data))
+diffSteadyState(lθ::SuperOperator, dlθ::SuperOperator, ρθ::AbstractOperator) = 
+    _solve(lθ; bop=-dlθ*ρθ, is_tp=true, trace=0)
 
-    lm = [lθ.data  similar(lθ.data, M^2); permutedims([(i ÷ M + 1) == i % M ? one(eltype(lθ.data)) : zero(eltype(lθ.data)) for i in 1:M^2+1])]
-
-    dρθ = bicgstabl!(x0, lm, y)[1:end-1] # Super vector
-    dρθ = Operator(lθ.basis_l[1], lθ.basis_l[2], reshape(dρθ, (M, M)))
-    return (dρθ + dagger(dρθ)) / 2.0
-end
-
-function sld(ρθ::AbstractOperator, dρθ::AbstractOperator)
-    M = length(ρθ.basis_l)
-    x0 = 1e-6 * rand(eltype(ρθ.data), M^2)
-
-    y = reshape(dρθ.data, M^2)
-    
-    lm = (spre(ρθ) + spost(ρθ)).data / 2.0
- 
-    sld = Operator(ρθ.basis_l, ρθ.basis_r, reshape(bicgstabl!(x0, lm * lm, lm * y), (M, M))) # Matrix
-
-    return (sld + dagger(sld)) / 2.0
-end
+sld(ρθ::AbstractOperator, dρθ::AbstractOperator) = _solve((spre(ρθ) + spost(ρθ))/2.0; bop=dρθ)
 
 """
     qfi(θ::Real, liouv::Function, dliouv::Function; indices=nothing, n_sld::Integer=2)
@@ -88,31 +71,21 @@ function qfi(θ::Real, liouv::Function, dliouv::Function; indices=nothing)
 end
 
 macro define(liouv, dliouv, indices)
-    if !(indices === nothing)
-        return quote
-            ss = (θ -> ptrace(steadyState(liouv(θ)), $indices))
-            dss = (θ -> ptrace(diffSteadyState($liouv(θ), $dliouv(θ), ss(θ)), $indices))
-            frule((_, Δx), ::typeof(ss), x) = begin
-                ss(x), dss(x)*Δx
-            end
-            rrule(::typeof(ss), θ) = begin
-                ss_pullback(Δy) = (NoTangent(),  dss(x) * Δy)
-                return ss(θ), ss_pullback
-            end
+    ss_expr = !(indices === nothing) ? 
+              :(ss = (θ -> ptrace(steadyState(liouv(θ)), $indices))) :
+              :(ss = (θ -> steadyState(liouv(θ))))
+    dss_expr = !(indices === nothing) ?
+        :(dss = (θ -> ptrace(diffSteadyState($liouv(θ), $dliouv(θ), ss(θ)), $indices))) :
+        :(dss = (θ -> diffSteadyState($liouv(θ), $dliouv(θ), ss(θ))))
+    return quote
+        $ss_expr
+        $dss_expr
+        frule((_, Δx), ::typeof(ss), x) = (ss(x), dss(x)*Δx)
+        rrule(::typeof(ss), θ) = begin
+            ss_pullback(Δy) = (NoTangent(),  dss(x) * Δy)
+            return ss(θ), ss_pullback
         end
-    else
-        return quote
-            ss = (θ -> steadyState(liouv(θ)))
-            dss = (θ -> diffSteadyState($liouv(θ), $dliouv(θ), ss(θ)))
-            frule((_, Δx), ::typeof(ss), x) = begin
-                ss(x), dss(x)*Δx
-            end
-            rrule(::typeof(ss), θ) = begin
-                ss_pullback(Δy) = (NoTangent(),  dss(x) * Δy)
-                return ss(θ), ss_pullback
-            end
-        end
-    end    
+    end
 end
 
 end
